6aabcfbcad88c8afecdab99d42ec8d3d63efa397
Lemon73 <Lemonade.Flash.1164@gmail.com>
2025-12-27 01:23:28 +0900

    tests(lib): create the sample cli-app to use himeno-lib it can runs on windows-x64

---
+++ src/HimenoBMTLib/Program.cs
@@ -0,0 +1,21 @@
+﻿using Himeno = HimenoBMT.HimenoBMT;
+
+namespace HimenoBMTLib;
+
+public static class Program
+{
+    public static int Main()
+    {
+        try
+        {
+            Himeno.Run(256);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e);
+            return 1;
+        }
+
+        return 0;
+    }
+}
